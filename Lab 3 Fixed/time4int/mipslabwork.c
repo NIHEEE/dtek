510fc188e54148597d1102a5ddecd9b5638b7b82
Niels Barth <nielsbarth@hotmail.com>
2023-10-02 11:15:08 +0200

    .

--- Lab 3 Fixed/time4int/mipslabwork.c
+++ Lab 3 Fixed/time4int/mipslabwork.c
@@ -4,57 +4,68 @@
    Updated 2017-04-21 by F Lundevall
 
    This file should be changed by YOU! So you must
-   add comment(s) here with your name(s) and date(s): Calin Capitanu 24/01/2019
-
-   This file modified 2017-04-31 by Ture Teknolog 
+   add comment(s) here with your name(s) and date(s):
 
    For copyright and licensing, see file COPYING */
 
 #include <stdint.h>   /* Declarations of uint_32 and the like */
 #include <pic32mx.h>  /* Declarations of system-specific addresses etc */
 #include "mipslab.h"  /* Declatations for these labs */
+
 int mytime = 0x5957;
-volatile int* trisE = (volatile int*) 0xbf886100;
-volatile int* portE = (volatile int*) 0xbf886110;
-char textstring[] = "text, more text, and even more text!";
-int timeoutcounter = 0;
+int timeoutcount = 0;
 int prime = 1234567;
+
+char textstring[] = "text, more text, and even more text!";
+
+volatile unsigned int* tris_e = (volatile unsigned int*) 0xbf886100;
+volatile unsigned int* port_e = (volatile unsigned int*) 0xbf886110;
 
 
 /* Interrupt Service Routine */
-void user_isr( void ) {
-  timeoutcounter++;
-  if(timeoutcounter == 9){
-    time2string( textstring, mytime );
-    display_string( 1, "");
-    display_string( 2, "");
+void user_isr( void )
+{
+  if((IFS(0) & 0x100) == 0x100){
+    timeoutcount++;
+    IFSCLR(0) = 0x100;
+  }
+
+  if (timeoutcount == 10){
+    time2string( textstring, mytime ); 
     display_string( 3, textstring );
     display_update();
-    
-    tick( &mytime );
-    timeoutcounter = 0;
+    tick(&mytime);
+    timeoutcount = 0;
   }
-  //if((IFS(0) >> 8) & 1)
-    IFSCLR(0) =  0x0100;
 }
+
 /* Lab-specific initialization goes here */
-void labinit( void )
-{
-  *trisE = ~(0xff) & *trisE;
-  TRISD = (0x7f << 5) | TRISD;
-  TMR2 = 0;
-  PR2 = 31250;
-  T2CON = 0x8070;
-  IEC(0) = 1 << 8;
-  IPC(2) = 7 << 2;
+void labinit( void ){
+  *tris_e = *tris_e & 0xffffff00;      //Set the 8 least significant bits of TRISE to 0
   
+  TRISDSET = 0x000007F0;               //port D: bits 11-5 are inputs (buttons & switches)
+
+  TMR2 = 0;                            //Initialize timer to 0
+  PR2 = 31250;                         //Peruid = 1/10 s, 80 000 000/256/5 = 31250
+  T2CONCLR = 0b1010;                   //Sets bit 1 to 0 (TCS) to use in-built timer and bit 3 to 0 for 16-bit timer. 
+  T2CONSET = 0x8070;                   //0b1000000001110000; /*sets bit 15 (ON) to 1 to turn on timer, bit 6 (TCKPS) to 1 for 1:256 prescaling and bit 4 (TGATE) to 1 for gated time accumulation
+  IFSCLR(0) = 0x100;                   //Clear the timer interrupt status flag   
+
   return;
 }
 
-/* This function is called repetitively from the main program */
 
-void labwork( void ) {
-  prime = nextprime( prime );
-  display_string( 0, itoaconv( prime ) );
+/* This function is called repetitively from the main program */
+void labwork( void ){
+  prime = nextprime(prime);
+  display_string(0, itoaconv(prime));
   display_update();
+
+  
 }
+
+
+
+
+
+
